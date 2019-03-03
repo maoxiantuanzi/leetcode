b4a818cbe78375fd4aefa0fb7746fb1aaafea4bf
pengtt0119 <vam_kindred@163.com>
2019-03-03 17:13:31 +0800

    Update 42.接雨水.cpp

--- 42.接雨水.cpp
+++ 42.接雨水.cpp
@@ -27,7 +27,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
+		stack<int> st;
+		int i = 0, res = 0, n = height.size();
+		while (i < n) {
+			if (st.empty() || height[i] <= height[st.top()]) {
+				st.push(i++);
+			}
+			else {
+				int t = st.top();
+				st.pop();
+				if (st.empty())	continue;
+				res += (min(height[i], height[st.top()]) - height[t]) * (i - st.top() - 1);
+			}
+		}
+		return res;
     }
 };
 
