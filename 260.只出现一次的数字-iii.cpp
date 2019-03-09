f44396695a746f9f55e850359f3ea31db5b4a133
pengtt0119 <vam_kindred@163.com>
2019-03-09 21:50:59 +0800

    Update 260.只出现一次的数字-iii.cpp

--- 260.只出现一次的数字-iii.cpp
+++ 260.只出现一次的数字-iii.cpp
@@ -29,7 +29,14 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        
+		int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+		diff &= -diff;
+		vector<int> res(2, 0);
+		for (auto &a : nums) {
+			if (a&diff)	res[0] ^= a;
+			else res[1] ^= a;
+		}
+		return res;
     }
 };
 
